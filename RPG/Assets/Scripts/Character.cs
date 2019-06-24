d01b6f0990b7ee45f0e14375bf3db2b71c1609d6
Haruta_prog <haruta.prog1999@gmail.com>
2019-06-24 11:43:51 +0900

    no message

---
+++ RPG/Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour
+{
+    public string name;
+
+    public int HP;
+    public int ATK;
+    public int DEF;
+
+    public UnityEvent[] Actions = new UnityEvent[6];
+}
---
+++ RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public string Equip_Weapon;
+    public string Equip_Body;
+    public string Equip_Accessories;
+}
