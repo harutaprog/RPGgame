a7f59dffa43f0ba34bff29edc901e8b0e46167c0
HarutaProg <haruta.prog1999@gmail.com>
2019-06-24 17:58:34 +0900

    no message

--- RPG/Assets/Scripts/Character.cs
+++ RPG/Assets/Scripts/Character.cs
@@ -5,11 +5,18 @@ using UnityEngine.Events;
 
 public class Character : MonoBehaviour
 {
-    public string name;
+    public string CharacterName;
 
     public int HP;
     public int ATK;
     public int DEF;
 
+    public int NowHP;
+    public int NowATK;
+    public int NowDEF;
+
     public UnityEvent[] Actions = new UnityEvent[6];
+
+    [SerializeField]
+    public List<Condition.conditionStruct> conditionLists;
 }
---
+++ RPG/Assets/Scripts/Condition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condition : MonoBehaviour
+{
+    [System.Serializable]
+    public enum conditionName
+    {
+        毒,
+        麻痺,
+        気絶,
+        攻撃力上昇,
+        防御力上昇,
+        攻撃力低下,
+        防御力低下,
+    }
+
+    [System.Serializable]
+    public class conditionStruct
+    {
+        public conditionName name;
+        public int turn;
+        public int value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
