865f837332de02bb45c0452d74ab04bc4e9b302f
春田直宏 <haruta.prog1999@gmail.com>
2019-06-28 12:29:04 +0900

    no message

--- RPG/Assets/Scripts/BattleCore.cs
+++ RPG/Assets/Scripts/BattleCore.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BattleCore : MonoBehaviour
 {
@@ -8,14 +9,16 @@ public class BattleCore : MonoBehaviour
     public List<Enemy> enemies;
     public EnemyList enemyList;
     public Action action;
-
+    public delegate void Del<T1,T2>(List<T1> attackSide, int attackChar, List<T2> defenceSide, int defenceChar);
     public int targetEnemy;
     // Start is called before the first frame update
     void Start()
     {
+        Del<Player,Enemy> del = action.Attack;
         enemies.Add(enemyList.enemyList[0]);
         enemies[0].NowHP = enemies[0].HP;
-        action.Attack<Player,Enemy>(players, 0, enemies, 0);
+        del(players, 0, enemies, 0);
+//        action.Attack(players, 0, enemies, 0);
     }
 
     // Update is called once per frame
